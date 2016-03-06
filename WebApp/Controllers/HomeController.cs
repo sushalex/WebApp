fe5c7f56ded39208c30608ed84dae2e3f1cf7833
sushalex <aleksandr.borissov@gmail.com>
2016-03-06 23:43:17 +0200

    Second

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            var result = "no data";
+            try
+            {
+                
+                using (var context = new azure_db())
+                {
+                    // Perform data access using the context 
+                    result = string.Join(",", context.Users.ToList().Select(user => user.UserId.ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                result = ex.ToString();
+            }
+            
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your about page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/Database.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class azure_db : DbContext
+    {
+        public DbSet<User> Users { get; set; } 
+    }
+
+    public class User { public int UserId { get; set; } }
+}
